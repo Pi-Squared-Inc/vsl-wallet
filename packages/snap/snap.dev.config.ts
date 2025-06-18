fb674038d90c7dda55bb99cdc46a3ad28da305c0
damienen <damian.ovidiu27@gmail.com>
2025-06-19 01:37:08 +0300

    deployment

--- packages/snap/snap.dev.config.ts
+++ packages/snap/snap.dev.config.ts
@@ -1,20 +1,20 @@
-import type { SnapConfig } from '@metamask/snaps-cli';
-import { resolve } from 'path';
+import type { SnapConfig } from "@metamask/snaps-cli";
+import { resolve } from "path";
 
 const config: SnapConfig = {
-  input: resolve(__dirname, 'src/index.ts'),
-  server: {
-    port: 8080,
-  },
-  polyfills: {
-    process: true,
-    buffer: true,
-    events: true,
-  },
-  // You must use 127.0.0.1 instead of localhost as node by default try ipv6 first
-  environment: {
-    VSL_ENDPOINT: 'http://127.0.0.1:44444',
-  },
+	input: resolve(__dirname, "src/index.ts"),
+	server: {
+		port: 8080,
+	},
+	polyfills: {
+		process: true,
+		buffer: true,
+		events: true,
+	},
+	// You must use 127.0.0.1 instead of localhost as node by default try ipv6 first
+	environment: {
+		VSL_ENDPOINT: "https://rpc.vsl.pi2.network",
+	},
 };
 
 export default config;
